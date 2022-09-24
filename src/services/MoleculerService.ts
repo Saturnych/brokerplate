dc928770499d60d29e821059e3cb15f27e1e57a5
Saturnych <saturnych@gmail.com>
2022-09-24 15:40:53 +0300

    perfhooks fix

--- src/services/MoleculerService.ts
+++ src/services/MoleculerService.ts
@@ -11,10 +11,12 @@ import { Context, Service, ServiceBroker, ServiceSchema } from 'moleculer';
 
 import { monitorEventLoopDelay } from 'perf_hooks';
 
+import { getPerfhookInfo } from '../utils';
+
 import { DEBUG, VERSION } from '../config/vars';
 
 const defaultEvent = (service: Service, state: string = '') => {
-	if (service.debug()) {
+	if (service?.debug()) {
 		service.logger?.info(`${service.name} event emitted`);
 	}
 	service.state(state);
@@ -24,7 +26,7 @@ export default class MoleculerService extends Service {
 	protected _debug: boolean = DEBUG;
 	protected _state: string = 'down';
 	protected _initial: Record<string, any> = {};
-	protected _perfhook: Record<string, any> = monitorEventLoopDelay({ resolution: 20 });
+	protected _perfhook = monitorEventLoopDelay({ resolution: 20 });
 	public name: string = 'service';
 
 	public constructor(params: any) {
@@ -32,13 +34,11 @@ export default class MoleculerService extends Service {
 		this.name = params.name;
 		const {
 			broker,
-			stopped = defaultEvent(this, 'down'),
-			created = defaultEvent(this),
-			started = defaultEvent(this),
-			afterConnected = defaultEvent(this),
+			stopped,
+			created,
+			started,
 			...schema
 		} = params;
-		this.state('up');
 		const eventsMask = `${this.name}.*`;
 		this.events = {
 			[eventsMask]: schema.events || {
@@ -47,10 +47,9 @@ export default class MoleculerService extends Service {
 			}
 		};
 		schema.events = this.events;
-		schema.afterConnected = afterConnected;
-		schema.created = created;
-		schema.started = started;
-		schema.stopped = stopped;
+		schema.created = created || this.serviceCreated;
+		schema.started = started || this.serviceStarted;
+		schema.stopped = stopped || this.serviceStopped;
 		schema.version = VERSION;
 		schema.meta = { scalable: true };
 		schema.dependencies = [];
@@ -65,15 +64,36 @@ export default class MoleculerService extends Service {
 		return this._initial;
 	}
 
-	public perfhook(): Record<string, any> {
-		this._perfhook = monitorEventLoopDelay({ resolution: 20 });
+	public perfhook(enabled: boolean | undefined): Record<string, any> {
+		if (String(enabled)==='true') this._perfhook.enable();
+		else if (String(enabled)==='false') this._perfhook.disable();
 		return this._perfhook;
   }
 
 	public state(st: string = ''): string {
-		if (!!st) {
-			this._state = st;
-		}
+		if (!!st) this._state = st;
 		return this._state;
 	}
+
+	public serviceCreated() {
+    if (this._debug) this.logger.info(`+++ ${this.name}.serviceCreated()`);
+    this.state('starting');
+		const perfhook = this.perfhook(true);
+		if (this._debug) this.logger.info(`+++ ${this.name}.perfhook:`, getPerfhookInfo(perfhook));
+    return ('serviceCreated' in this.events)?this.events.serviceCreated(this):null;
+  }
+
+  public serviceStarted() {
+    if (this._debug) this.logger.info(`+++ ${this.name}.serviceStarted()`);
+    this.state('up');
+    return ('serviceStarted' in this.events)?this.events.serviceStarted(this):null;
+  }
+
+  public serviceStopped() {
+    if (this._debug) this.logger.info(`--- ${this.name}.serviceStopped()`);
+    this.state('down');
+		const perfhook = this.perfhook(false);
+    if (this._debug) this.logger.info(`--- ${this.name}.perfhook:`, getPerfhookInfo(perfhook));
+    return ('serviceStopped' in this.events)?this.events.serviceStopped(this):null;
+  }
 }
