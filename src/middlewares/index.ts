68c4352675ed2c57216a82e7c241f5a4f73a3059
Denis Glebko <denis.glebko@gmail.com>
2023-01-20 23:26:54 +0300

    Update index.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -12,5 +12,16 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import serveStatic from 'serve-static';
+import { DEBUG } from '../config/vars';
 
-export default [compression(), cookieParser(), helmet(), serveStatic(path.join(__dirname, '../../public'))];
+export default [
+	compression(),
+	cookieParser(),
+	helmet(),
+	serveStatic(path.join(__dirname, '../../public')),
+	function (err, req, res, next) {
+		const ctx = res.$ctx;
+		if (DEBUG) ctx.logger.error('Error is occured in middlewares!');
+		ctx.sendError(req, res, err);
+	},
+];
